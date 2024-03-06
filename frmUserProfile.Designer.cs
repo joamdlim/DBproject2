a661bc8329d1a115c8ce227f241d7cc72cefe017
jmlim159@gmail.com <jmlim159@gmail.com>
2024-03-06 13:31:10 +0800

    Add project files.

---
+++ frmUserProfile.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace DBPROJECT
+{
+    partial class frmUserProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmUserProfile));
+            this.cbxGender = new System.Windows.Forms.ComboBox();
+            this.txtLoginName = new System.Windows.Forms.TextBox();
+            this.pkrBirthdate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSMTPHOST = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSMTPport = new System.Windows.Forms.TextBox();
+            this.btnChangePwd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnLoadPhoto = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.pictBoxUser = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxUser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxGender
+            // 
+            this.cbxGender.FormattingEnabled = true;
+            this.cbxGender.Items.AddRange(new object[] {
+            "MALE",
+            "FEMALE"});
+            this.cbxGender.Location = new System.Drawing.Point(142, 493);
+            this.cbxGender.Name = "cbxGender";
+            this.cbxGender.Size = new System.Drawing.Size(121, 21);
+            this.cbxGender.TabIndex = 0;
+            this.cbxGender.TextChanged += new System.EventHandler(this.cbxGender_TextChanged);
+            // 
+            // txtLoginName
+            // 
+            this.txtLoginName.AcceptsReturn = true;
+            this.txtLoginName.AcceptsTab = true;
+            this.txtLoginName.Location = new System.Drawing.Point(142, 347);
+            this.txtLoginName.Name = "txtLoginName";
+            this.txtLoginName.ReadOnly = true;
+            this.txtLoginName.Size = new System.Drawing.Size(208, 20);
+            this.txtLoginName.TabIndex = 2;
+            // 
+            // pkrBirthdate
+            // 
+            this.pkrBirthdate.CustomFormat = "MM/dd/yyyy";
+            this.pkrBirthdate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.pkrBirthdate.Location = new System.Drawing.Point(142, 463);
+            this.pkrBirthdate.Name = "pkrBirthdate";
+            this.pkrBirthdate.Size = new System.Drawing.Size(200, 20);
+            this.pkrBirthdate.TabIndex = 3;
+            this.pkrBirthdate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.pkrBirthdate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EnableSavebutton);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Login Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 469);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Birthdate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 411);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "SMTP Host";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "SMTP Port";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 496);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Gender";
+            // 
+            // txtSMTPHOST
+            // 
+            this.txtSMTPHOST.Location = new System.Drawing.Point(142, 408);
+            this.txtSMTPHOST.Name = "txtSMTPHOST";
+            this.txtSMTPHOST.Size = new System.Drawing.Size(208, 20);
+            this.txtSMTPHOST.TabIndex = 10;
+            this.txtSMTPHOST.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EnableSavebutton);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(142, 376);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(208, 20);
+            this.txtEmail.TabIndex = 11;
+            this.txtEmail.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EnableSavebutton);
+            // 
+            // txtSMTPport
+            // 
+            this.txtSMTPport.Location = new System.Drawing.Point(142, 434);
+            this.txtSMTPport.Name = "txtSMTPport";
+            this.txtSMTPport.Size = new System.Drawing.Size(99, 20);
+            this.txtSMTPport.TabIndex = 12;
+            this.txtSMTPport.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EnableSavebutton);
+            // 
+            // btnChangePwd
+            // 
+            this.btnChangePwd.Image = ((System.Drawing.Image)(resources.GetObject("btnChangePwd.Image")));
+            this.btnChangePwd.Location = new System.Drawing.Point(31, 585);
+            this.btnChangePwd.Name = "btnChangePwd";
+            this.btnChangePwd.Size = new System.Drawing.Size(172, 44);
+            this.btnChangePwd.TabIndex = 13;
+            this.btnChangePwd.Text = "Change User Password";
+            this.btnChangePwd.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnChangePwd.UseVisualStyleBackColor = true;
+            this.btnChangePwd.Click += new System.EventHandler(this.btnChangePwd_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.Location = new System.Drawing.Point(271, 585);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(111, 33);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Image = ((System.Drawing.Image)(resources.GetObject("btnRefresh.Image")));
+            this.btnRefresh.Location = new System.Drawing.Point(442, 596);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(113, 23);
+            this.btnRefresh.TabIndex = 15;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnLoadPhoto
+            // 
+            this.btnLoadPhoto.Image = ((System.Drawing.Image)(resources.GetObject("btnLoadPhoto.Image")));
+            this.btnLoadPhoto.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLoadPhoto.Location = new System.Drawing.Point(114, 292);
+            this.btnLoadPhoto.Name = "btnLoadPhoto";
+            this.btnLoadPhoto.Size = new System.Drawing.Size(166, 35);
+            this.btnLoadPhoto.TabIndex = 16;
+            this.btnLoadPhoto.Text = "Load New Photo";
+            this.btnLoadPhoto.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLoadPhoto.UseVisualStyleBackColor = true;
+            this.btnLoadPhoto.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
+            this.btnClear.Location = new System.Drawing.Point(341, 292);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(132, 35);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Clear Photo";
+            this.btnClear.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // pictBoxUser
+            // 
+            this.pictBoxUser.Location = new System.Drawing.Point(189, 12);
+            this.pictBoxUser.Name = "pictBoxUser";
+            this.pictBoxUser.Size = new System.Drawing.Size(246, 274);
+            this.pictBoxUser.TabIndex = 18;
+            this.pictBoxUser.TabStop = false;
+            // 
+            // frmUserProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 674);
+            this.Controls.Add(this.pictBoxUser);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLoadPhoto);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnChangePwd);
+            this.Controls.Add(this.txtSMTPport);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtSMTPHOST);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pkrBirthdate);
+            this.Controls.Add(this.txtLoginName);
+            this.Controls.Add(this.cbxGender);
+            this.Name = "frmUserProfile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmUserProfile_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxUser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxGender;
+        private System.Windows.Forms.TextBox txtLoginName;
+        private System.Windows.Forms.DateTimePicker pkrBirthdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSMTPHOST;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSMTPport;
+        private System.Windows.Forms.Button btnChangePwd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnLoadPhoto;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox pictBoxUser;
+    }
+}
